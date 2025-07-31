27d21c96d0c8f474aed966da40ae3c2f39dbf556
Chris Malek <cmalek@caltech.edu>
2025-07-31 11:39:10 -0700

    BUGFIX: had wrong version number in doc/source/conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -28,7 +28,7 @@ copyright: str = "Christopher Malek 2025"  # noqa: A001
 author: str = "Christopher Malek"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.2"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
